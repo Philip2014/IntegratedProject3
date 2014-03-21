239ed0338051d40fbfdab3bf8aafd96bf36ed6af
Stephen McVeigh <stevo86_mcv@live.co.uk>
2014-03-21 13:05:30 +0000

    added title screen

--- Journey to find Judo George/Assets/Scripts/SelectLevelScript.cs
+++ Journey to find Judo George/Assets/Scripts/SelectLevelScript.cs
@@ -2,12 +2,19 @@ using UnityEngine;
 using System.Collections;
 
 public class SelectLevelScript : MonoBehaviour {
-
+	
+	public Transform level1;
+	public Transform level2;
+	
 	bool isTriggered = false;
 	string levelSelect;
+	
+	Color badColor = new Color(150,0,0,0.4f);
 	// Use this for initialization
 	void Start () {
 	
+		level1.renderer.enabled = false;
+		level2.renderer.enabled = false;
 	}
 	
 	// Update is called once per frame
@@ -51,6 +58,40 @@ public class SelectLevelScript : MonoBehaviour {
 
 	void OnTriggerExit(Collider other)
 	{
-		other.renderer.material.color = Color.white;
+		other.renderer.material.color = badColor;
+	}
+	
+	void OnMouseDown()
+	{
+		if(gameObject.tag == "level1")
+		{
+			Application.LoadLevel("TheClassroom");
+		}
+		
+		else if(gameObject.tag == "level2")
+		{
+			Application.LoadLevel("TheClassroom");
+		}
+		
+	}
+	
+	void OnMouseEnter()
+	{
+		
+		if(gameObject.tag == "level1")
+		{
+			level1.renderer.enabled = true;
+		renderer.material.color = badColor;
+		}
+		else if(gameObject.tag == "level2")
+		{
+			level2.renderer.enabled = true;
+		renderer.material.color = badColor;
+		}
+	}
+	void OnMouseExit()
+	{
+		level1.renderer.enabled = false;
+		level2.renderer.enabled = false;
 	}
 }
